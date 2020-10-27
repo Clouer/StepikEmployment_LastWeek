c1abdc31b29f28b76880c2762ded6529071a5156
Clouer <alex.pyhtya@gmail.com>
2020-10-27 18:08:02 +0300

    Companies owners added, ApplicationResponse model added

--- recruiting/models.py
+++ recruiting/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -7,6 +9,7 @@ class Company(models.Model):
     logo = models.CharField(max_length=150)
     description = models.CharField(max_length=400)
     employee_count = models.IntegerField()
+    owner = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
 
 
 class Specialty(models.Model):
@@ -17,10 +20,18 @@ class Specialty(models.Model):
 
 class Vacancy(models.Model):
     title = models.CharField(max_length=100)
-    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE, related_name="vacancies")
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="vacancies")
+    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE, related_name='vacancies')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='vacancies')
     skills = models.CharField(max_length=150)
     description = models.CharField(max_length=400)
     salary_min = models.IntegerField()
     salary_max = models.IntegerField()
     published_at = models.DateField()
+
+
+class ApplicationResponse(models.Model):
+    written_username = models.CharField(max_length=100)
+    written_phone = models.CharField(max_length=12)
+    written_cover_letter = models.CharField(max_length=300)
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, related_name='applications')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='applications')
